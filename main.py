a42ba35dae0cad192a9bec48624aae32e9a89317
Threepix <98532056+Threepix@users.noreply.github.com>
2022-09-22 22:59:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,64 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
-from datetime import datetime
 import lxml
 import pandas as pd
 import openpyxl
+from selenium_stealth import stealth
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+
+driver = webdriver.Chrome(options=options,
+                          executable_path="C:\\Users\\Иван\\PycharmProjects\\pythonProject1\\chromedriver\\chromedriver.exe")
+
+stealth(
+    driver,
+    languages=["en-Us", "en"],
+    vendor="Google Inc.",
+    platform="Win64",
+    webgl_vendor="Intel Inc.",
+    renderer="Intel Iris OpenGL Engine",
+    fix_hairline=True
+)
 
 
-url = "https://www.tradingview.com/markets/stocks-usa/market-movers-high-dividend/"
+def get_mvid():
+    url = "https://www.mvideo.ru/product-list-page?q=lightning+%D0%BA%D0%B0%D0%B1%D0%B5%D0%BB%D1%8C&category=kabeli-dlya-sotovyh-telefonov-344"
+    uri = "https://www.mvideo.ru/product-list-page?q=%D0%BA%D0%B0%D0%B1%D0%B5%D0%BB%D0%B8+usb+type-c&category=344"
+    try:
+        driver.get(url)
+        time.sleep(7)
+        resMvideo = []
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        data = soup.find_all("span", {"class": "price__main-value"})
+        for x in range(len(data)):
+            resMvideo.append("".join(list(data[x])).replace("\xa0", ""))
+        print(resMvideo)
 
-def connect_1():
-    res = []
-    koti=[]
-    links =[]
-    lasti=[]
-    epss=[]
-    cap = []
-    link = "https://tradingview.com"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content,"html.parser")
-    naz = soup.find_all("sup",{"class":"apply-common-tooltip tickerDescription-qN79lDF8"})
-    kot = soup.find_all("a",{"class":"apply-common-tooltip tickerName-qN79lDF8"})
-    last = soup.find_all("td",{"class":"cell-v9oaRE4W right-v9oaRE4W"})
-    for x in range(100):
-        res.append("".join(list(naz[x])))
-        koti.append("".join(list(kot[x])))
-    for a in soup.find_all('a',{"class":"apply-common-tooltip tickerName-qN79lDF8"},href=True):
-        post=link + a['href']
-        links.append(post)
-        post = link
-    for bio in links:
-        otvet = requests.get(bio)
-        back = BeautifulSoup(otvet.content, "html.parser")
-        eps = back.find_all("div", {"class": " tv-fundamental-block__value js-symbol-eps "})
-        epss.append(eps)
-        capital = back.find_all("span", {"class": "tv-widget-fundamentals__value apply-overflow-tooltip"})
-        cap.append(capital)
-    df = pd.DataFrame({"Company":res,"Kotirovka":koti,"Links":links})
-    df.to_excel('./teams.xlsx')
-    print(epss)
-    print(cap)
-    return kot
+        driver.get(uri)
+        time.sleep(7)
+        resc=[]
+        replit = driver.page_source
+        sop = BeautifulSoup(replit, "lxml")
+        dat = sop.find_all("span", {"class": "price__main-value"})
+        for x in range(len(dat)):
+            resc.append("".join(list(dat[x])).replace("\xa0", ""))
+        print(resc)
+        df = pd.DataFrame({"Mvideo ligthting": resMvideo})
+        dt = pd.DataFrame({"Mvideo TYPE c": resc})
 
+        df.to_excel('./mvid.xlsx')
+
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 if __name__ == "__main__":
-    print(connect_1())
+    print(get_mvid())
+    print("a")
